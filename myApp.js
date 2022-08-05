af93af9309d2eb3c275e9bb1ab6ddb8b697abdb1
ajwad masood <ajwadmasood@ajwads-MacBook-Air.local>
2022-08-05 18:42:00 -0500

    first commit

--- myApp.js
+++ myApp.js
@@ -1,17 +1,22 @@
 let express = require('express');
 let app = express();
+// const http = require('http')
+// const server = http.createServer()
 
-console.log('Hello World')
+// console.log('Hello World')
+
+// app.get('/',function(req, res) {
+//     res.send("Hello Express");
+//   })
+
+// absolutePath = __dirname + relativePath/file.ext
 
 app.get('/',function(req, res) {
-    res.send("Hello Express");
+    res.sendFile('/Users/ajwadmasood/Desktop/boilerplate-express/views/index.html');
   })
+// app.get('/', '/Users/ajwadmasood/Desktop/boilerplate-express/views/index.html')
 
-absolutePath = __dirname + /views/index.html
-// /Users/ajwadmasood/Desktop/boilerplate-express/views/index.html
-// app.get('/', /Users/ajwadmasood/Desktop/boilerplate-express/views/index.html)
-
-
+// app.listen(5000)
 
 
 
