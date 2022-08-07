15e4e5c2083708c9f71b47546b61bfe684f50a54
ajwad masood <ajwadmasood@ajwads-MacBook-Air.local>
2022-08-06 19:50:49 -0500

    api

--- myApp.js
+++ myApp.js
@@ -3,6 +3,15 @@ let app = express();
 const path = require('path')
 require('dotenv').config();
 
+const logger = (req, res, next) => {
+    const method = req.method
+    const path = req.path
+    const ip = req.ip
+    console.log(method, path, ip)
+    next()
+  }
+app.use(logger)
+
 app.use('/public',express.static(path.resolve(__dirname, 'public')))
 // app.use(express.static('/Users/ajwadmasood/Desktop/boilerplate-express/public'));
 // app.use('/public', express.static('./public'));
@@ -15,7 +24,6 @@ app.use('/public',express.static(path.resolve(__dirname, 'public')))
 // absolutePath = __dirname + /views/index.html
 const absolute = path.resolve(__dirname, 'views', 'index.html')
 
-
 app.get('/',function(req, res) {
     res.sendFile(absolute);
   })
@@ -23,19 +31,13 @@ app.get('/',function(req, res) {
 
 app.get('/json',function(req, res) {
     if (process.env.MESSAGE_STYLE=='uppercase'){
-        console.log('dscd')
         return res.json({"message": "Hello json".toUpperCase()});
     }
     res.json({"message": "Hello json"});
   })
 
-// if (process.env.MESSAGE_STYLE=='uppercase'){
-//     app.get('/json', function(req, res) {
-//     return res.json({ "message": "HELLO JSON" })
-//     });
-// }
 
-// app.listen(5000)
+app.listen(5000)
 
 
 
